adb9315c6534154abc6db526770244e12e8558dc
jawon22 <winwkdnjs@naver.com>
2023-09-02 18:55:13 +0900

    세미전 연습하자!

---
+++ practiceSemi/src/main/java/com/example/demo/PracticeSemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSemiApplication.class, args);
+	}
+
+}
